66df5c207452e034476bc904b3bf0a16f52bcb7d
Noodles <yih022@ucsd.edu>
2022-06-16 22:13:44 -0700

    finish feed view sketch

--- Twitter_Clone/Core/Components/Tweets/TweetRowView.swift
+++ Twitter_Clone/Core/Components/Tweets/TweetRowView.swift
@@ -10,16 +10,75 @@ import SwiftUI
 struct TweetRowView: View {
     var body: some View {
         VStack {
-            HStack(alignment: .top, spacing: 12) {
-                Circle()
-                    .frame(width: 56, height: 56)
-                    .foregroundColor(Color(.systemBlue))
-                
-                VStack {
+            VStack (alignment: .leading) {
+                // Avatar + user info + tweet
+                HStack(alignment: .top, spacing: 12) {
+                    Circle()
+                        .frame(width: 56, height: 56)
+                        .foregroundColor(Color(.systemBlue))
+                    
+                    // User Info and tweet caption
+                    VStack (alignment: .leading, spacing: 4) {
+                        // User Info
+                        HStack {
+                            Text("Bruce Sotthin")
+                                .font(.subheadline).bold()
+                            
+                            Text("@batman")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                            
+                            Text("2w")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
+                        
+                        // Tweet Caption
+                        Text("This is a brand new tweet!")
+                            .font(.subheadline)
+                            .multilineTextAlignment(.leading)
+                    }
                     
                 }
+    //            .padding()
+                
+                // Action Buttons
+                HStack {
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "bubble.left")
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "arrow.2.squarepath")
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "heart")
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "bookmark")
+                            .font(.subheadline)
+                    }
+                }
+                .padding()
             }
+            .padding()
+            Divider()
         }
+        
+       
     }
 }
 
